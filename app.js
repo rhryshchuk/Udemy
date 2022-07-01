8ab778618219300e939e840bf974e138d6d776b2
Roman Hryshchuk <rhryshchik3332@gmail.com>
2022-07-02 00:31:25 +0300

    commited practice 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "Use strict";
 
 
-let numberOfFilms = +prompt("Скільки фільмів ви глянули", " ");
+ let numberOfFilms = +prompt("Скільки фільмів ви глянули", " ");
 let personalMovieD = {
     count: numberOfFilms,
     movies: {},
@@ -9,11 +9,35 @@ let personalMovieD = {
     genres: [],
     privat: false
 };
-
-let a = prompt("Один з осанніх фільмів", " "),
-    b = prompt("Оцінка фільму", " "),
-    c = prompt("Один з осанніх фільмів", " "),
-    d = prompt("Оцінка фільму", " ");
-personalMovieD.movies[a] = b;
+ let a = prompt("Один з осанніх фільмів", " "),
+     b = prompt("Оцінка фільму", " "),
+     c = prompt("Один з осанніх фільмів", " "),
+     d = prompt("Оцінка фільму", " ");
+ personalMovieD.movies[a] = b;
 personalMovieD.movies[c] = d;
+ 
+for (let i = 0; i < 2; i++) {
+     let a = prompt("Один з осанніх фільмів", " "),
+        b = prompt("Оцінка фільму", " ");
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieD.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+        
+   
+}   
+if (personalMovieD.count < 10) {
+    console.log('мало фільмів');
+} else if (personalMovieD.count >= 10 && personalMovieD.count <= 30 ) {
+    console.log('нормально фільмів');
+} else if (personalMovieD.count >= 30) {
+    console.log('very good');
+} else {
+    console.log('error');
+}
 console.log(personalMovieD);
+ 
